b10a0dd7e53ea515ab0aacefb49b8f883905c4a8
beni <benigno.cantero@gmail.com>
2017-12-05 12:45:27 +0100

    tercer comit codigo del segundo ejercicio

--- Cadena.java
+++ Cadena.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Cadena{
+
+	public static void main (String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.printf("Introduce una palabra%n");
+		String cadenaleida = sc.nextLine ();
+		sc.close();	
+		String cadenaLeidaMayuscula = cadenaleida.toUpperCase();
+		String cadenaLeidaMinuscula = cadenaleida.toLowerCase();
+		System.out.printf("La cadena leida \"%s\" es equivalente en minusculas a %s , y en mayusculas a %s%n",cadenaleida,cadenaLeidaMinuscula,cadenaLeidaMayuscula);	
+	}
 }
